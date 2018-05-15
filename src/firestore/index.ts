bb6f2cf69b638acb66501db39e41d5ac6f3cc7c7
Andras Eszes <andras.eszes@gmail.com>
2018-05-15 17:52:38 +0200

    Fix production Firebase init.json fetching error

--- src/firestore/index.ts
+++ src/firestore/index.ts
@@ -7,7 +7,7 @@ import { isProduction } from '@/lib/environment'
 export const initialize = () => {
   if (isProduction()) {
     return fetch('/__/firebase/init.json').then((response) => {
-      return firebase.initializeApp(response.json())
+      return response.json().then(firebase.initializeApp)
     })
   } else {
     return Promise.resolve(firebase.initializeApp(config.firebase ? config.firebase : {}))
